9d45143ee22316810fa268c1c837304c6beb1973
Crow475 <116875872+Crow475@users.noreply.github.com>
2022-12-13 13:55:51 +0400

    Add docstrings and refactor
    - Added docstrings to the Table class and its methods
    - Added docstring to the document_reader function
    - Renaned 'table' class to 'Table' to better comly with PEP 8
    - Made some lines shorter to comply with PEP 8

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import csv
 alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
-class table:
+class Table:
+    """
+    A class that stores values of any type in a table,
+    limited to 26 columns (for now)
+    """
     def __init__(self, column_count, row_count):
         self.column_count = column_count
         self.row_count = row_count
@@ -9,19 +13,23 @@ class table:
         for column in range(1, self.column_count + 1):
             column_alpha = alphabet[column - 1]
             self.columns[column_alpha] = [None for i in range(1, self.row_count + 1)]
-    
+
     def get_cell(self, column: str, row: int):
+        """Returns the value stored in a cell"""
         return self.columns[column][row - 1]
 
     def set_cell(self, column: str, row: int, value):
+        """Sets the value of a cell"""
         self.columns[column][row  - 1] = value
 
     def add_column(self):
+        """Adds an empty column to the right of the table"""
         new_column_letter = alphabet[self.column_count]
         self.columns[new_column_letter] = [None for i in range(1, self.row_count + 1)]
         self.column_count += 1
-    
+
     def add_row(self):
+        """Adds an empty row to the bottom of the table"""
         self.row_count += 1
         for column in range(1, self.column_count + 1):
             column_alpha = alphabet[column - 1]
@@ -33,11 +41,13 @@ class table:
             return_string = return_string + " | "
             for j in range(1, self.column_count + 1):
                 j_alpha = alphabet[j - 1]
-                return_string = return_string + str(self.get_cell(j_alpha, i)) + " | "
+                return_string = (return_string
+                + str(self.get_cell(j_alpha, i)) + " | ")
             return_string = return_string + "\n"
         return return_string
 
 def file_opener(file_name):
+    """Opens a csv file as a table object"""
     with open(file_name, encoding="utf-8") as file:
         filereader = csv.reader(file)
         max_rows = 0
@@ -47,7 +57,7 @@ def file_opener(file_name):
                 max_columns = len(row)
             max_rows = max_rows + 1
 
-        return_table = table(max_columns, max_rows)
+        return_table = Table(max_columns, max_rows)
 
         file.seek(0)
         current_row = 0
@@ -56,5 +66,5 @@ def file_opener(file_name):
             for column, value in enumerate(row):
                 column_alpha = alphabet[column]
                 return_table.set_cell(column_alpha, current_row, value)
-    
+
     return return_table
